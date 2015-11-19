98a49c6c0db6f575ec217b08c8fdd6c72cd1def8
John Austin <JohnAustin-MSFT@users.noreply.github.com>
2015-11-18 19:45:05 -0800

    Update Constants.java
    Added "_" character between words of place holder text for client id and redirect uri

--- app/src/main/java/com/microsoft/office365/connectmicrosoftgraph/Constants.java
+++ app/src/main/java/com/microsoft/office365/connectmicrosoftgraph/Constants.java
@@ -6,8 +6,8 @@ package com.microsoft.office365.connectmicrosoftgraph;
 interface Constants {
     String AUTHORITY_URL = "https://login.microsoftonline.com/common";
     // Update these two constants with the values for your application:
-    String CLIENT_ID = "ENTER YOUR CLIENT ID";
-    String REDIRECT_URI = "ENTER YOUR REDIRECT URL";
+    String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
+    String REDIRECT_URI = "ENTER_YOUR_REDIRECT_URI";
     String MICROSOFT_GRAPH_API_ENDPOINT = "https://graph.microsoft.com/v1.0/";
     String MICROSOFT_GRAPH_API_ENDPOINT_RESOURCE_ID = "https://graph.microsoft.com/";
 }

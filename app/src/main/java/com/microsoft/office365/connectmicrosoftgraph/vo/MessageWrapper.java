36efa1d078aa9be43c5579cdc0c3e134d21b411a
Brian Melton <brianmel@microsoft.com>
2015-11-19 17:05:53 -0800

    Unused constructor gone

--- app/src/main/java/com/microsoft/office365/connectmicrosoftgraph/vo/MessageWrapper.java
+++ app/src/main/java/com/microsoft/office365/connectmicrosoftgraph/vo/MessageWrapper.java
@@ -4,17 +4,12 @@
  */
 package com.microsoft.office365.connectmicrosoftgraph.vo;
 
-import android.annotation.SuppressLint;
-
 import com.google.gson.annotations.SerializedName;
 
 public class MessageWrapper {
 
     @SerializedName("Message")
     public MessageVO mMessage;
-
-    @SuppressLint("unused")
-    public MessageWrapper() {}
 
     public MessageWrapper(MessageVO msg) {
         mMessage = msg;
